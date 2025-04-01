3167ada399d78ea7d72f7b00646a56289289af67
Sovichet-Rathanak <rathanaksovichet@gmail.com>
2025-04-01 18:04:33 +0700

    config target

--- src/main/java/itc/gic/i4d/App.java
+++ src/main/java/itc/gic/i4d/App.java
@@ -1,5 +1,8 @@
 package itc.gic.i4d;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Hello world!
  */
@@ -12,6 +15,17 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Create the frame
+        JFrame frame = new JFrame("Hello JFrame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+        frame.setLayout(new BorderLayout());
+        
+        // Create a label with text
+        JLabel label = new JLabel("Hello, World!", SwingConstants.CENTER);
+        frame.add(label, BorderLayout.CENTER);
+        
+        // Make the frame visible
+        frame.setVisible(true);
     }
 }
